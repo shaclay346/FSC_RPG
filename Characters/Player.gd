0e0295e88a9e42eae72a2e53e18aa2dcc26099ac
shaclay346 <shaclay346@gmail.com>
2023-03-23 22:22:15 -0400

    reworked project structure

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -21,16 +21,16 @@ func set_house(new_house):
 func read_input():
 	velocity = Vector2()
 	
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 		direction = Vector2(0,1)
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 		direction = Vector2(0,1)
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 		direction = Vector2(-1,0)
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 		direction = Vector2(1,0)	
 	
